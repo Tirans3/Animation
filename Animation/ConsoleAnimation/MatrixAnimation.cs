733db5e6314df803f101b5f22e885315d2383fd2
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-10 16:54:41 +0400

    Add  necessary filds

--- Animation/ConsoleAnimation/MatrixAnimation.cs
+++ Animation/ConsoleAnimation/MatrixAnimation.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAnimation
 {
     class MatrixAnimation
     {
+        readonly char b = '*';
+        readonly int m = Console.CursorLeft;
+        readonly int n = Console.CursorTop;
+        readonly int y = 0;
+        readonly int z = 0;
+        readonly static char[,] A = new char[15, 100];
     }
 }
